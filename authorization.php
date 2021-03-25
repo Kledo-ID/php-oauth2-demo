702551000cf79078cf5753566bfc9ad6c377a02d
Panji Setya Nur Prawira <kstar.panjinamjaelf@gmail.com>
2021-03-25 12:30:18 +0700

    feat: Authorization

---
+++ authorization.php
@@ -0,0 +1,23 @@
+<?php
+
+require 'load.php';
+
+use Josantonius\Session\Session;
+
+$state = random_string(40);
+
+Session::set('state', $state);
+
+$query = http_build_query([
+    'client_id' => $_ENV['CLIENT_ID'],
+    'redirect_uri' => $_ENV['REDIRECT_URI'],
+    'response_type' => 'code',
+    'scope' => '',
+    'state' => $state,
+]);
+
+$authorizationUrl = $_ENV['API_HOST'].'/authorize?'.$query;
+
+header('Location: '.$authorizationUrl);
+
+exit();
